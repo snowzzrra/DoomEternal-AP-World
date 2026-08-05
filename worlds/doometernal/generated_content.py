c4fe72a83ce9964a1fc8f147b3adb73942e6a22c
snowzzrra <guimotta7@gmail.com>
2026-08-05 01:41:54 -0300

    DOOMEternal: 0.3.9: version bump

--- worlds/doometernal/generated_content.py
+++ worlds/doometernal/generated_content.py
@@ -2,7 +2,7 @@
 
 # This file has no timestamp: stable input yields a stable diff.
 CONTENT_SCHEMA_VERSION = 2
-CONTENT_REVISION = 'v0.3.8-alpha.2'
+CONTENT_REVISION = 'v0.3.9-alpha'
 BRIDGE_PROTOCOL_VERSION = 3
 
 LOCATION_ROWS = (
